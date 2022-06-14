6791bccec51f2f6c084a05950bed70213c350201
Hunseong <phunseong@gmail.com>
2022-06-14 14:52:24 +0900

    add: server project initial commit

---
+++ post-sample-server/src/main/java/com/hunseong/postsample/PostSampleApplication.java
@@ -0,0 +1,13 @@
+package com.hunseong.postsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostSampleApplication.class, args);
+	}
+
+}
---
+++ post-sample-server/src/test/java/com/hunseong/postsample/PostSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hunseong.postsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
